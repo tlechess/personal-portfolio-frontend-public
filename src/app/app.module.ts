036899866ae417d57777b33fe5d964dce8593f06
Tyler <tjltestes@gmail.com>
2021-12-20 19:15:23 -0500

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import {MatListModule} from '@angular/material/list'; 
+import {MatButtonModule} from '@angular/material/button';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavBarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatListModule,
+    MatButtonModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
